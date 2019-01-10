76a2a3bb25c3db475ff68f01a6bf4353be65a265
Gwenaël Galon <gwenael.gallon@mac.com>
2019-01-11 00:45:17 +0100

    Add Document extented

---
+++ src/app/pages/_document.js
@@ -0,0 +1,23 @@
+import Document, { Head, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <html lang="en">
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+          <meta name="theme-color" content="#000" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+};
