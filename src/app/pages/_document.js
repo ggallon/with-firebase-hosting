14ccfa8cf149c08982094164fdd0955dc73a6dca
Gwenaël Galon <gwenael.gallon@mac.com>
2019-01-11 01:40:03 +0100

    Add charset

--- src/app/pages/_document.js
+++ src/app/pages/_document.js
@@ -10,6 +10,7 @@ export default class MyDocument extends Document {
     return (
       <html lang="en">
         <Head>
+          <meta charset="utf-8">
           <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
           <meta name="theme-color" content="#000" />
         </Head>
