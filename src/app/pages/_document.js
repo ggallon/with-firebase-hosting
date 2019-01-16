e2b3e366f8b262434b6c7bbf16d234c2dfccbf80
Gwenaël Gallon <ggallon@users.noreply.github.com>
2019-01-16 10:23:22 +0100

    (clore) add meta for msapplication-TileColor

--- src/app/pages/_document.js
+++ src/app/pages/_document.js
@@ -13,13 +13,14 @@ export default class MyDocument extends Document {
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
           <meta name="description" content="Firebase Next Hosting" />
-          <meta name="theme-color" content="#ffffff" />
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
           <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
           <link rel="manifest" href="/_next/static/manifest.json" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff" />
         </Head>
         <body>
           <Main />
